5887e07ff3b30cb4b0320423c9e8db83d72d12d6
Corey Griffin <iamcoreyg@gmail.com>
2020-06-02 09:12:03 -0700

    update

--- src/App.js
+++ src/App.js
@@ -5,20 +5,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <li><a href="https://docs.google.com/spreadsheets/u/0/d/1cPKzlmzp-KIDEeR3IXe14lB_kzlj_acHCvWluQRN--w/htmlview#gid=0">Austin</a></li>
+        <li><a href="https://docs.google.com/spreadsheets/u/1/d/1mTthE5lwqVnTCIm3iQtQXLyxwK-pc17cuCp--BhAYX8/htmlview">Bay Area</a></li>
+        <li><a href="https://docs.google.com/spreadsheets/d/18w-0RBhwBBlXDN9kRV9DVSCAGSCjtHb9K0Pq2YBv18U/htmlview?pru=AAABcpmcwrE*x3y6soEcsPyTjQ3lrCrL9w#gid=943359524">Los Angeles</a></li>
+        <li><a href="https://docs.google.com/spreadsheets/d/1EJz0wThvDiOuSS54FHL4qZhO7qJeEuljf01Ez3EPR8k/edit#gid=0"></a></li>
+        <li><a href="https://docs.google.com/spreadsheets/d/1NiDhMfKLcAoVFQpI5g-AQH945_WpmR_bbu7OAgSU-R8/htmlview">Philedelphia</a></li>
+        <li><a href="https://docs.google.com/spreadsheets/d/1Q3C90fKE7krH4Ot-GoS5EWcnLbvtBVbedC04rjvnDxk/htmlview?pru=AAABcpUXe4k*yckyqTvB8sDY_NvIyhFL2A">New York</a></li>
+        <li><a href="https://docs.google.com/document/d/1TvvsmN0H3dfClTYuXh5qMqYCdH6o7b0bxANoDzasoKQ/edit">Maryland</a></li>
+        <li><a href="https://docs.google.com/document/d/10l7NItyZUoNroE4iLePGn5GAvSuQ7KW5IK4XmOcDSTI/mobilebasic">Toronto</a></li>
+      </ul>
     </div>
   );
 }
